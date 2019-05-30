d016d57dd6dc9b20d5e041cc3e57944aa7ac5670
a.malinovskiy <a.malinovskiy@cian.ru>
2019-05-30 12:42:46 +0300

    add settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+PROXY = {
+    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn', 
+        'password': 'python'
+    }
+}
+
+API_KEY = "886440728:AAEWAxQ7haqJ3wjvQZJZe6Sv4Q2NUsLsC7Y"	
+
+USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
