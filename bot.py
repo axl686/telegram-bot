f4ebb080e4e81ee567e0c0ad21e265ffdbb66390
a.malinovskiy <a.malinovskiy@cian.ru>
2019-05-30 15:46:33 +0300

    cars and keyboards

--- bot.py
+++ bot.py
@@ -4,11 +4,24 @@ import logging
 
 from glob import glob
 from random import choice
-
 from emoji import emojize
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler
 
 import settings
+
+PROXY = {
+    'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {
+        'username': 'learn', 
+        'password': 'python'
+    }
+}
+
+API_KEY = "886440728:AAEWAxQ7haqJ3wjvQZJZe6Sv4Q2NUsLsC7Y"   
+
+USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -19,14 +32,14 @@ def greet_user(bot, update, user_data):
     emo = emojize(choice(settings.USER_EMOJI), use_aliases=True)
     user_data['emo'] = emo
     text = 'Привет {}'.format(emo)
-    update.message.reply_text(text)
+    update.message.reply_text(text, reply_markup=get_keyboard())
 
 def talk_to_me(bot, update, user_data):
     emo = get_user_emo(user_data)
     user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, emo, update.message.text)
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, update.message.chat.id, 
-                        update.message.text)
-    update.message.reply_text(user_text)
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, 
+                        update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text, reply_markup=get_keyboard())
 
 def get_const_planet(bot, update, user_data):
     planet = split(text)[1]
@@ -38,12 +51,28 @@ def get_const_planet(bot, update, user_data):
       pl = ephem.planet(now.strftime("%Y/%m/%d"))
       const = ephem.constellation(pl)
       print(const)
-      update.message.reply_text(const)
+      update.message.reply_text(const, reply_markup=get_keyboard())
 
 def get_car_picture(bot, update, user_data):
     car_list = glob('images/*.jp*g')
     car_picture = choice(car_list)
-    bot.send_photo(chat_id=update.message.chat.id, photo=open(car_picture, 'rb'))
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(car_picture, 'rb'), reply_markup=get_keyboard())
+
+def change_avatar(bot, update, user_data):
+    if 'emo' in user_data:
+        del user_data['emo']
+    emo = get_user_emo(user_data)
+    update.message.reply_text('Done:'.format(emo), reply_markup=get_keyboard())
+
+def get_contact(bot, update, user_data):
+    print(update.message.contact)
+    emo = get_user_emo(user_data)
+    update.message.reply_text('Done:'.format(emo), reply_markup=get_keyboard())
+
+def get_location(bot, message, user_data):
+    print(update.message.location)
+    emo = get_user_emo(user_data)
+    update.message.reply_text('Done:'.format(emo), reply_markup=get_keyboard())
 
 def get_user_emo(user_data):
     if 'emo' in user_data:
@@ -52,14 +81,29 @@ def get_user_emo(user_data):
         user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
         return user_data['emo']
 
+def get_keyboard():
+    contact_button = KeyboardButton('Send contacts', request_contact=True)
+    location_button = KeyboardButton('Send coordinates', request_location=True)
+    my_keyboard = ReplyKeyboardMarkup([
+                                        ['Прислать тачку', 'Сменить аватар']
+                                        [contact_button, location_button]
+                                       ], resize_keyboard=True
+                                    )
+    return my_keyboard
+
 def main():
     #learn1_axl_bot
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
-    dp.add_handler(CommandHandler("car", get_car_picture))
-    dp.add_handler(CommandHandler("planet", get_const_planet))
+    dp.add_handler(CommandHandler("car", get_car_picture, pass_user_data=True))
+    dp.add_handler(CommandHandler("planet", get_const_planet, pass_user_data=True))
 
+    dp.add_handler(RegexHandler('^(Прислать тачку)$', get_car_picture, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(Сменить аватар)$', change_avatar, pass_user_data=True))
+
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     mybot.start_polling()
