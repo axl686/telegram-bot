7c0a6312471c46e5cc412913eeb6c8af2b4b0607
a.malinovskiy <a.malinovskiy@cian.ru>
2019-06-10 22:52:45 +0300

    add is_car in utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
+from random import choice
+
+from clarifai.rest import ClarifaiApp
 from emoji import emojize
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 import settings
+
 
 def get_user_emo(user_data):
     if 'emo' in user_data:
@@ -9,6 +13,7 @@ def get_user_emo(user_data):
     else:
         user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
         return user_data['emo']
+
 
 def get_keyboard():
     contact_button = KeyboardButton('Send contacts', request_contact=True)
@@ -20,3 +25,18 @@ def get_keyboard():
                                         ], resize_keyboard=True
                                     )
     return my_keyboard
+
+
+def is_car(file_name):
+    image_has_car = False
+    app = ClarifaiApp(api_key=settings.CLARIFAI_API_KEY)
+    model = app.public_models.general_model
+    response = model.predict_by_filename(file_name, max_concepts=5)
+    if response['status']['code'] == 10000:
+        for concept in response['outpets'][0]['data']['concepts']:
+            if concept['name'] == 'car':
+                image_has_car = True
+    return image_has_car
+
+if __name__ == '__main__':
+    print(is_car('images/images.jpeg'))
