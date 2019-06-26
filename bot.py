2c8e0f9ea6a6087f6db0d8a33a329ce8785296a5
a.malinovskiy <a.malinovskiy@cian.ru>
2019-06-26 17:48:04 +0300

    add alarm and message limit

--- bot.py
+++ bot.py
@@ -1,21 +1,38 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
+from telegram.ext import messagequeue as mq
 
 from handlers import *
 import settings
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+)
+
+subscribers = set()
+
+
+def my_test(bot, job):
+    bot.sendMessage(chat_id=840851, text='Lovely Spam! Wonderful Spam!')
+    job.interval += 5
+    if job.interval > 20:
+        bot.sendMessage(chat_id=840851, text='By!')
+        job.schedule_removal()
 
 
 def main():
     #learn1_axl_bot
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+    mybot.bot._msg_queue = mq.MessageQueue()
+    mybot.bot._is_message_queued_default = True
 
-    logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO,
-                        filename='bot.log'
-    )
+    logging.info('Bot starting')
 
     dp = mybot.dispatcher
+
+    mybot.job_queue.run_repeating(send_updates, interval=5) #interval in seconds
 
     anketa = ConversationHandler(
         entry_points = [RegexHandler('^(Fill the form)$', form_start, pass_user_data=True)],
@@ -36,12 +53,15 @@ def main():
     dp.add_handler(anketa)
     dp.add_handler(CommandHandler("car", get_car_picture, pass_user_data=True))
     dp.add_handler(CommandHandler("planet", get_const_planet, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(Send me a cool car)$', get_car_picture, pass_user_data=True))
-    dp.add_handler(RegexHandler('^(Change avatar)$', change_avatar, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(send me a cool car)$', get_car_picture, pass_user_data=True))
+    dp.add_handler(RegexHandler('^(change avatar)$', change_avatar, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
     dp.add_handler(MessageHandler(Filters.location, get_location, pass_user_data=True))
+    dp.add_handler(CommandHandler('subscribe', subscribe))
+    dp.add_handler(CommandHandler('unsubscribe', unsubscribe))
+    dp.add_handler(CommandHandler('alarm', set_alarm, pass_args=True, pass_job_queue=True))
+
     dp.add_handler(MessageHandler(Filters.photo, check_user_photo, pass_user_data=True))
-    
     dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
     
     mybot.start_polling()
